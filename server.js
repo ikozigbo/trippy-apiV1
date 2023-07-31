6d1c146dbd96f14d98159829ccb024e45b9814c0
ikozigbo <97337028+ikozigbo@users.noreply.github.com>
2023-07-31 10:40:10 +0100

    CORS installation

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 require("./config/dbConfig");
 const express = require("express");
+const cors = require("cors");
 const userRouter = require("./routers/userRouter");
 const adminRouter = require("./routers/adminRouter");
 const PORT = process.env.PORT || 5050;
 
 const app = express();
+
+// Enable CORS for all routes
+app.use(cors());
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
