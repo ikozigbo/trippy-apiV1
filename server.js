2c5429d369ab0153356ff8ef105f814d7eebca1f
ikozigbo <97337028+ikozigbo@users.noreply.github.com>
2023-08-04 12:22:27 +0100

    update

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const morgan = require("morgan");
 const fileUpload = require("express-fileupload");
 const userRouter = require("./routers/userRouter");
 const adminRouter = require("./routers/adminRouter");
+const hotelRouter = require("./routers/hotelRouter");
+const tourRouter = require("./routers/tourRouter");
 const PORT = process.env.PORT || 5050;
 
 const app = express();
@@ -26,6 +28,8 @@ app.get("/", (req, res) => {
 });
 app.use("/trippy", userRouter);
 app.use("/trippy", adminRouter);
+app.use("/trippy", hotelRouter);
+app.use("/trippy", tourRouter);
 
 app.listen(PORT, () => {
   console.log(`listening to port ${PORT}`);
