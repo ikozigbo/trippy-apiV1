d71c0a5ad4c93da5ba5cac9486e3f5e647f97ec4
ikozigbo <97337028+ikozigbo@users.noreply.github.com>
2023-08-23 22:36:35 +0100

    good job so far

--- controller/bookingController.js
+++ controller/bookingController.js
@@ -3,6 +3,10 @@ const Booking = require("../model/bookingModel");
 const QRCode = require("qrcode");
 const { generateBookingEmail } = require("../utilities/bookingConfirmation");
 const User = require("../model/userModel");
+const Hotel = require("../model/hotelModel");
+const Tour = require("../model/tourModel");
+const Car = require("../model/carRentalModel");
+const Flight = require("../model/flightModel");
 
 // const qrCode = async (text) => {
 //   try {
@@ -14,6 +18,7 @@ const User = require("../model/userModel");
 
 const createBooking = async (req, res) => {
   try {
+    const { _id } = req.user;
     const {
       flightId,
       returnFlightId,
@@ -37,9 +42,53 @@ const createBooking = async (req, res) => {
       totalPrice,
     } = req.body;
 
-    console.log(checkOutDate);
-    const { _id } = req.user;
     const user = await User.findById(_id);
+
+    const firstname = user.firstName;
+    let hotelName = "no hotel booked";
+    let hotelAddress = "-";
+    let checkinDate = "-";
+    let checkoutDate = "-";
+    let numRooms = "-";
+    let flightDepature = "-";
+    let flightArrival = "-";
+    let travelers = "-";
+    let returnFlightDepature = "-";
+    let returnFlightArrival = "-";
+    let carRentalDetails = "-";
+    let tourDetails = "-";
+    const price = tourPrice;
+
+    //console.log(checkOutDate);
+
+    if (hotelId) {
+      const hotel = await Hotel.findById(hotelId);
+      hotelName = hotel.hotelName;
+      hotelAddress = `${hotel.address}, ${hotel.city}, ${hotel.country}`;
+      checkinDate = checkInDate;
+      checkoutDate = checkOutDate;
+      numRooms = numberOfRooms;
+    }
+    if (tourId) {
+      const tour = await Tour.findById(tourId);
+      tourDetails = `${tour.tourName}, ${tour.city}, ${tour.country}`;
+    }
+    if (flightId) {
+      const flight = await Flight.findById(flightId);
+      flightDepature = ` ${flight.from}, ${flight.depatureAirport}, ${flight.depatureTime}, ${flightDate}`;
+      flightArrival = ` ${flight.to}, ${flight.arrivalAirport}, ${flight.arrivalTime}, ${flightDate}`;
+
+      travelers = namesOfTravelers;
+    }
+    if (returnFlightId) {
+      const returnFlight = await Flight.findById(returnFlightId);
+      returnFlightDepature = ` ${returnFlight.from}, ${returnFlight.depatureAirport}, ${returnFlight.depatureTime}, ${returnDate}`;
+      returnFlightArrival = ` ${returnFlight.to}, ${returnFlight.arrivalAirport}, ${returnFlight.arrivalTime}, ${returnDate}`;
+    }
+    if (carRentalId) {
+      const carRental = await Car.findById(carRentalId);
+      carRentalDetails = `${carRental.brand}, ${carRental.model}, ${carRental.color}, rental registration: ${carRental.registrationNumber}, rental date: ${rentalDate}, for: ${rentalDays} days`;
+    }
     const booking = {
       user: _id,
       tourBooking: {
@@ -73,11 +122,29 @@ const createBooking = async (req, res) => {
     };
 
     const newBooking = await Booking.create(booking);
+    console.log(newBooking);
+    const refNum = newBooking._id;
     // const code = await qrCode(`https://trippy-huas.onrender.com`);
     //console.log(code);
     const subject = "Booking Confirmation";
     const link = `https://trippy-huas.onrender.com`;
-    const html = await generateBookingEmail(user.firstName);
+    const html = await generateBookingEmail(
+      firstname,
+      hotelName,
+      hotelAddress,
+      checkinDate,
+      checkoutDate,
+      numRooms,
+      flightDepature,
+      flightArrival,
+      travelers,
+      returnFlightDepature,
+      returnFlightArrival,
+      carRentalDetails,
+      tourDetails,
+      price,
+      refNum
+    );
     console.log(html);
     const data = {
       email: user.email,
